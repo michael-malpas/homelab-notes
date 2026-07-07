d92b748ed8289b1fccc63e15824ae67acd7f2284
Michael Malpas <michael.r.malpas@gmail.com>
2026-07-07 19:16:35 +0000

    updating workflow/environments/terraform/testing

--- terraform-ci-demo/terraform/backend.tf
+++ terraform-ci-demo/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 	backend "s3" {
 		bucket = "michael-malpas-terraform-state-2026"
-		key = "terraform/day19/terraform.tfstate"
+		key = "terraform/day23/terraform.tfstate"
 		region = "us-east-1"
 		use_lockfile = true
 	}
