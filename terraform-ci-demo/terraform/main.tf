5f8241ad45eefe66827a456e57b5bc9a06bf058e
Michael Malpas <michael.r.malpas@gmail.com>
2026-07-10 17:08:21 +0000

    Fixing Terraform yml formatting

--- terraform-ci-demo/terraform/main.tf
+++ terraform-ci-demo/terraform/main.tf
@@ -55,14 +55,14 @@ module "web" {
   source = "./modules/ec2"
 
   # Pass required variables here
-  instance_count = var.instance_count
-  ami_id = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  key_name = var.key_name
-  user_data = file("${path.module}/userdata.sh")
-  security_group_id= aws_security_group.web.id
-  server_name = var.server_name
-  environment = var.environment
+  instance_count    = var.instance_count
+  ami_id            = data.aws_ami.ubuntu.id
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  user_data         = file("${path.module}/userdata.sh")
+  security_group_id = aws_security_group.web.id
+  server_name       = var.server_name
+  environment       = var.environment
 }
 
 resource "local_file" "ansible_inventory" {
