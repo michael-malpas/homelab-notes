e3bf3bcf3448754f8d887ace07100e7dc7fd960f
Michael Malpas <michael.r.malpas@gmail.com>
2026-07-15 18:27:23 +0000

    Updating terraform and workflow for environment isolation

--- terraform-ci-demo/terraform/backend.tf
+++ terraform-ci-demo/terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket       = "michael-malpas-terraform-state-2026"
-    key          = "terraform/day24/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
+  backend "s3" {}
 }
